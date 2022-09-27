2f44379d072b2503c253e19ccfc54cd8547de2ea
Romil003 <romilghadge33@gmail.com>
2022-09-27 21:08:44 +0530

    Added Employee Wage UC3

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,15 +1,21 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-    static int EMP_WAGE_PER_HOUR = 20;
-    static final int DAILY_TIME_HOUR = 8;
+    static final int EMP_WAGE_PER_HOUR = 20;
+    static int FULL_TIME_HOUR = 8;
+    static int PART_TIME_HOUR = 4;
     public static void main(String[] args) {
-        int employeeCheck = (int)Math.floor(Math.random()*10) %2;
+        int employeeCheck = (int)Math.floor(Math.random()*10) %3;
         System.out.println(employeeCheck);
         int dailyWage = 0;
-        if(employeeCheck == 1){
+        if(employeeCheck == 2){
             System.out.println("Employee is present");
-            dailyWage = EMP_WAGE_PER_HOUR*DAILY_TIME_HOUR;
+            dailyWage = EMP_WAGE_PER_HOUR*FULL_TIME_HOUR;
+            System.out.println("Daily wage is " + dailyWage);
+        }
+        else if (employeeCheck == 1) {
+            System.out.println("Employee is present");
+            dailyWage = EMP_WAGE_PER_HOUR * PART_TIME_HOUR;
             System.out.println("Daily wage is " + dailyWage);
         }
         else {
