d7fca55244a0e93b7e1462d7cdf9db5a518b991d
Romil003 <romilghadge33@gmail.com>
2022-09-27 20:49:45 +0530

    Added Employee Wage UC1

---
+++ src/com/bridgelabz/EmployeeWage.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        int employeeCheck = (int)Math.floor(Math.random()*10) %2;
+        System.out.println(employeeCheck);
+        if(employeeCheck == 1){
+            System.out.println("Employee is present");
+        }
+        else {
+            System.out.println("Employee is absent");
+        }
+    }
+}
