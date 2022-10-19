a06ddec11294208f843fcc081423d9922cafb8e3
Romil003 <romilghadge33@gmail.com>
2022-10-19 23:43:38 +0530

    Added Employee Wage UC10

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 
-public class EmployeeWage {
+class CompanyEmployeeWage {
     private int EMP_WAGE_PER_HOUR ;
     private int FULL_TIME_HOUR ;
     private int PART_TIME_HOUR ;
@@ -11,17 +11,17 @@ public class EmployeeWage {
     static final int IS_PART_TIME = 2;
 
 
-    public EmployeeWage() {
+    public CompanyEmployeeWage() {
     }
 
-    public EmployeeWage(int EMP_WAGE_PER_HOUR, int FULL_TIME_HOUR, int PART_TIME_HOUR, int WORKING_DAYS_PER_MONTH) {
+    public CompanyEmployeeWage(int EMP_WAGE_PER_HOUR, int FULL_TIME_HOUR, int PART_TIME_HOUR, int WORKING_DAYS_PER_MONTH) {
         this.EMP_WAGE_PER_HOUR = EMP_WAGE_PER_HOUR;
         this.FULL_TIME_HOUR = FULL_TIME_HOUR;
         this.PART_TIME_HOUR = PART_TIME_HOUR;
         this.WORKING_DAYS_PER_MONTH = WORKING_DAYS_PER_MONTH;
     }
 
-    public void calculateEmployeeWage() {
+    public int calculateEmployeeWage() {
 
         int empHours = 0 ;
         int day = 1;
@@ -29,45 +29,67 @@ public class EmployeeWage {
         while (day < WORKING_DAYS_PER_MONTH && empHours <= 100) {
             int dailyWage = 0;
             int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
-            System.out.println(employeeCheck);
+//            System.out.println(employeeCheck);
             switch (employeeCheck) {
                 case IS_FULL_TIME:
-                    System.out.println("Employee is present");
+//                    System.out.println("Employee is present");
                     dailyWage = EMP_WAGE_PER_HOUR * FULL_TIME_HOUR;
                     empHours += FULL_TIME_HOUR ;
 
                     break;
                 case IS_PART_TIME:
-                    System.out.println("Employee is present");
+//                    System.out.println("Employee is present");
                     dailyWage = EMP_WAGE_PER_HOUR * PART_TIME_HOUR;
                     empHours += PART_TIME_HOUR ;
 
                     break;
                 default:
-                    System.out.println("Employee is absent");
+//                    System.out.println("Employee is absent");
             }
             day++;
             totalWage += dailyWage ;
-            System.out.println("Total employee hours are " + empHours);
-            System.out.println("Total wage for " + day + " day is " + totalWage);
+//            System.out.println("Total employee hours are " + empHours);
+//            System.out.println("Total wage for " + day + " day is " + totalWage);
         }
-        TOTAL_EMPLOYEE_WAGE = totalWage;
+//        System.out.println(totalWage);
+        return totalWage;
 
     }
 
-    @Override
-    public String toString() {
-        return "TOTAL_EMPLOYEE_WAGE=" + TOTAL_EMPLOYEE_WAGE ;
-    }
+
+
+//    public static void main(String[] args) {
+//        EmployeeWage LT = new EmployeeWage(30,10,6,25);
+//        LT.calculateEmployeeWage();
+//
+//        EmployeeWage Google = new EmployeeWage(20,11,7,23);
+//        Google.calculateEmployeeWage();
+//
+//    }
+}
+
+class EmpWageBuilder {
 
     public static void main(String[] args) {
-        EmployeeWage LT = new EmployeeWage(30,10,6,25);
-        LT.calculateEmployeeWage();
-        System.out.println("LT wage is : " + LT.toString());
+        CompanyEmployeeWage company1 = new CompanyEmployeeWage(30,10,6,25);
+        CompanyEmployeeWage company2 = new CompanyEmployeeWage(20,11,7,23);
+        CompanyEmployeeWage company3 = new CompanyEmployeeWage(25,12,8,28);
+        CompanyEmployeeWage company4 = new CompanyEmployeeWage(60,14,10,30);
 
-        EmployeeWage Google = new EmployeeWage(20,11,7,23);
-        Google.calculateEmployeeWage();
-        System.out.println("Google wage is : " + Google.toString());
+        int[] companyEmpWage = new int[4];
+        companyEmpWage[0] = company1.calculateEmployeeWage();
+        companyEmpWage[1] = company2.calculateEmployeeWage();
+        companyEmpWage[2] = company3.calculateEmployeeWage();
+        companyEmpWage[3] = company4.calculateEmployeeWage();
+
+        System.out.print("{ ");
+        for (int i=0; i<companyEmpWage.length;i++){
+            System.out.print(companyEmpWage[i] + " ");
+        }
+        System.out.print("}");
+
 
     }
 }
+
+
